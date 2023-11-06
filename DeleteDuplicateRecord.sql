673afe73d3ff4d937c3634a07c538cca7d8884ae
kaushik173 <108447588+kaushik173@users.noreply.github.com>
2023-11-06 13:03:26 +0530

    add second method to delete duplicate row

--- DeleteDuplicateRecord.sql
+++ DeleteDuplicateRecord.sql
@@ -7,3 +7,15 @@ select * from cte where rowNo <= 1
 ;with cte as
 (select *,ROW_NUMBER() over (Partition by FirstName order by FirstName) as rowNo from Student)
 delete from cte where rowNo > 1
+
+--#delete duplicate row from the contacts table
+delete from contacts where id in
+(select id from (
+SELECT 
+	id, 
+    email, 
+    ROW_NUMBER() OVER ( 
+		PARTITION BY email 
+        ORDER BY email
+	) AS row_num 
+FROM contacts)t where row_num > 1);
